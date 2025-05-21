e9784572522df0f0fdaf9aff6b4a8c410e5d2001
Maziar Sanjabi <maziar.sanjabi@gmail.com>
2025-05-21 08:26:09 +0000

    fully functional

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 torchrun --nproc-per-node=8 src/train_fsdp.py\
-    --epochs 3 \
-    --batch_size 8 \
+    --epochs 10 \
+    --batch_size 4 \
     --max_length 512 \
-    --lr 1e-5 \
-    --beta 0.1 \
+    --lr 1e-6 \
+    --beta 0.01 \
     --seed 2003 \
     --model_name "microsoft/phi-2" \
     --dataset_name "jondurbin/truthy-dpo-v0.1" \
