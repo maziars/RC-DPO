54685ed8b7cf14ff9103a88acd5e660f53bfadf7
Maziar Sanjabi <maziar.sanjabi@gmail.com>
2025-05-22 20:45:31 +0000

    working version that seems correct

---
+++ run-v2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+torchrun --nproc-per-node=1 src/train_fsdp-edited.py\
+    --epochs 10 \
+    --batch_size 4 \
+    --max_length 512 \
+    --lr 1e-5 \
+    --beta 0.01 \
+    --seed 2003 \
+    --model_name "microsoft/phi-2" \
+    --dataset_name "jondurbin/truthy-dpo-v0.1" \
+    --wandb_project "truthy-dpo"
