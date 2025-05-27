1377117ebbe196cfefaa4152f157ebf0f5a01279
Maziar Sanjabi <maziar.sanjabi@gmail.com>
2025-05-27 03:00:35 +0000

    working setting

--- run-fsdp-mh-fused.sh
+++ run-fsdp-mh-fused.sh
@@ -5,11 +5,11 @@ torchrun --nproc-per-node=1 src/train-fsdp-mh-fused.py\
     --batch_size 16 \
     --max_length 1024 \
     --lr 1e-5 \
-    --betas 0.5 0.4 0.6 0.45 0.55\
+    --betas 0.5 0.55 0.45 0.4 0.6 \
     --seed 2003 \
     --model_name "microsoft/phi-1_5" \
     --dataset_name "jondurbin/truthy-dpo-v0.1" \
     --wandb_project "truthy-dpo" \
-    --reg_weight 1e-4 \
+    --reg_weight 1e-3 \
     --wandb_enable True \
     --eval_ratio 0.9
