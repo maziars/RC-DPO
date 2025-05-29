1e86a5e094cc4ac21bb3850b6a91062735374111
Maziar Sanjabi <maziar.sanjabi@gmail.com>
2025-05-29 07:47:27 +0000

    margin loss

---
+++ run-fsdp-mh-fused-SV.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+torchrun --nproc-per-node=1 src/train-fsdp-mh-fused-SV.py\
+    --epochs 10 \
+    --batch_size 16 \
+    --max_length 1024 \
+    --lr 1e-5 \
+    --betas 0.5 0.55 0.45 0.4 0.6 \
+    --seed 2003 \
+    --model_name "microsoft/phi-1_5" \
+    --dataset_name "jondurbin/truthy-dpo-v0.1" \
+    --wandb_project "truthy-dpo" \
+    --reg_weight 1e-4 \
+    --wandb_enable True \
+    --eval_ratio 0.9 \
+    --margin 6.0
