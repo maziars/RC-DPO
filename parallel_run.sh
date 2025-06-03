cd9279eef3f17c51573be633d48b785302f9a8b5
Maziar Sanjabi <maziar.sanjabi@gmail.com>
2025-06-03 19:35:25 +0000

    fixing the parallel run

--- parallel_run.sh
+++ parallel_run.sh
@@ -1,10 +1,12 @@
-cat args.txt | parallel --colsep ' ' --jobs 1 --results logs_1/ '
+cat args.txt | parallel --colsep ' ' --jobs 8 --results logs/ '
   export CUDA_VISIBLE_DEVICES={%};
   lr={1}; margin={2}; reg={3};
+  port=$((29500 + {%}));
   outdir="checkpoints/lr=${lr}_margin=${margin}_reg=${reg}";
   mkdir -p "$outdir";
-  echo "Saving to $outdir";
-  torchrun --nproc-per-node=1 src/train-fsdp-mh-fused-SV.py \
+  echo "GPU {%} using port $port and saving to $outdir";
+
+  torchrun --nproc-per-node=1 --master-port $port src/train-fsdp-mh-fused-SV.py \
     --epochs 10 \
     --batch_size 16 \
     --max_length 1024 \
@@ -13,7 +15,7 @@ cat args.txt | parallel --colsep ' ' --jobs 1 --results logs_1/ '
     --seed 2003 \
     --model_name "microsoft/phi-1_5" \
     --dataset_name "jondurbin/truthy-dpo-v0.1" \
-    --wandb_project "truthy-dpo" \
+    --wandb_project "truthy-dpo-parallel" \
     --reg_weight $reg \
     --wandb_enable True \
     --eval_ratio 0.9 \
